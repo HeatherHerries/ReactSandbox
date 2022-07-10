d7ceaf4ef4f43934b7b3b12aafcaa4e114d49c9a
HeatherH <hherries@yahoo.com>
2022-07-10 07:51:36 -0600

    Built a function component

---
+++ SectionOne/08_FunctionComponent/index.js
@@ -0,0 +1,11 @@
+function Hello() {
+  return (
+    <div>
+      <h1>Hello There!</h1>
+      <h1>My Name is Heather!</h1>
+      <p>How are you today?</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<Hello />, document.getElementById("root"));
