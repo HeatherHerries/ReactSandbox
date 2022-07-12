42ae345cdb41c822dbaf2d7f9bb30d33229d826b
HeatherH <hherries@yahoo.com>
2022-07-11 20:27:44 -0600

    Default Props

---
+++ SectionThree/22_DefaultProps/index.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello to="Ringo" from=" Paul" bangs={4} />
+
+        <Hello to="Cher" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
