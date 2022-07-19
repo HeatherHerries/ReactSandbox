d9711ae577a019375e76a61c0403f8baa8e767ab
HeatherH <hherries@yahoo.com>
2022-07-19 12:29:54 -0600

    The difference between super() and super(props)

---
+++ Section Six/45_super-vs-super-props/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Game from "./Game";
+import Demo from "./Demo";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Game color="purple" />
+        <Demo animal="Bobcat" food="Pineapple" />
+      </div>
+    );
+  }
+}
+
+export default App;
