89985f95d163e7c1b36c85bdaf8f39a791aee175
HeatherH <hherries@yahoo.com>
2022-07-21 18:33:20 -0600

    Added animation to the dice when being rolled to finish project

--- Section Seven/dice-exercise/src/Die.js
+++ Section Seven/dice-exercise/src/Die.js
@@ -3,7 +3,13 @@ import "./Die.css";
 
 class Die extends Component {
   render() {
-    return <i className={`die fas fa-dice-${this.props.face}`}></i>;
+    return (
+      <i
+        className={`die fas fa-dice-${this.props.face} ${
+          this.props.rolling ? "shake" : ""
+        }`}
+      ></i>
+    );
   }
 }
 
